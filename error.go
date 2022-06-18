aa088cb318afd60deed167356a2d8938cacccad9
bytedream <bytedream@protonmail.com>
2022-06-19 01:42:57 +0200

    Fix error printing caused panic

--- error.go
+++ error.go
@@ -12,6 +12,6 @@ type RequestError struct {
 	Message  string
 }
 
-func (re *RequestError) String() string {
+func (re *RequestError) Error() string {
 	return fmt.Sprintf("error for endpoint %s (%d): %s", re.Response.Request.URL.String(), re.Response.StatusCode, re.Message)
 }

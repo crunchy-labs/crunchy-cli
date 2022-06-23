a283ba724785d537c56ce3967dd0f1b2fe489290
bytedream <bytedream@protonmail.com>
2022-06-23 14:27:15 +0200

    Add functions to get wallpaper urls

--- wallpaper.go
+++ wallpaper.go
@@ -1,4 +1,14 @@
 package crunchyroll
 
+import "fmt"
+
 // Wallpaper contains a wallpaper name which can be set via Account.ChangeWallpaper.
 type Wallpaper string
+
+func (w *Wallpaper) TinyUrl() string {
+	return fmt.Sprintf("https://static.crunchyroll.com/assets/wallpaper/360x115/%s", *w)
+}
+
+func (w *Wallpaper) BigUrl() string {
+	return fmt.Sprintf("https://static.crunchyroll.com/assets/wallpaper/1920x400/%s", *w)
+}

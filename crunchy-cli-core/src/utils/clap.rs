d880d641f094edc870575648a116896763cd3f29
ByteDream <bytedream@protonmail.com>
2023-04-11 21:14:06 +0200

    Add proxy flag (#142)

--- crunchy-cli-core/src/utils/clap.rs
+++ crunchy-cli-core/src/utils/clap.rs
@@ -1,6 +1,11 @@
 use crate::utils::parse::parse_resolution;
 use crunchyroll_rs::media::Resolution;
+use reqwest::Proxy;
 
 pub fn clap_parse_resolution(s: &str) -> Result<Resolution, String> {
     parse_resolution(s.to_string()).map_err(|e| e.to_string())
+}
+
+pub fn clap_parse_proxy(s: &str) -> Result<Proxy, String> {
+    Proxy::all(s).map_err(|e| e.to_string())
 }
